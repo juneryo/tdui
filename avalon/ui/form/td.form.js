67d7fb29bd68dea2526624c33850a04536ac5841
juneryo <juneryo@qq.com>
2015-10-13 10:50:41 +0800

    add load

--- avalon/ui/form/td.form.js
+++ avalon/ui/form/td.form.js
@@ -4,7 +4,9 @@ define(['avalon', '../base/js/mmRequest', 'text!./td.form.html', 'css!./td.form.
 	avalon.component("td:form", {
 		//外部属性
 		title: '',
-		url: '',
+		submitUrl: '',
+		loadUrl: '',
+		loadParam: {},
 		submit: true,  //是否存在submit按钮
 		reset: true,   //是否存在reset按钮
 		//外部参数
@@ -12,7 +14,7 @@ define(['avalon', '../base/js/mmRequest', 'text!./td.form.html', 'css!./td.form.
 		resetCallback: null,
 		errorCallback: null,
 		//view属性
-
+		isLoading: false,
 		//view接口
 		doSubmit: _interface,
 		doReset: _interface,
@@ -31,32 +33,74 @@ define(['avalon', '../base/js/mmRequest', 'text!./td.form.html', 'css!./td.form.
 			elem.innerHTML = elem.textContent = '';
 		},
 		$init: function(vm, elem) {
-			
-			vm.doSubmit = function(ev) {
-				if(vm.url != '') {
+			//内部方法
+			vm._trigger = function(ev, type) {
+				switch (type) {
+					case 'loaded': 
+						if(typeof vm.onloaded == 'function') {
+							vm.onloaded(ev, vm);
+						}
+						break;
+					default: break;
+				}
+			}
+			vm._callback = function(ev, type, data) {
+				switch (type) {
+					case 'submit': 
+						if(vm.submit === true && typeof vm.submitCallback == 'function') {
+							vm.submitCallback(ev, vm);
+						}
+						break;
+					case 'reset':
+						if(vm.reset===true && typeof vm.resetCallback == 'function') {
+							vm.resetCallback(ev, vm);
+						}
+						break;
+					case 'error':
+						if(typeof vm.errorCallback == 'function') {
+							vm.errorCallback(ev, vm, data);
+						}
+						break;
+					default: break;
+				}
+			}
+			vm._ajax = function(url, data, successCallback) {
+				if(url != '') {
+					vm.isLoading = true;
 					req.ajax({
 						type: 'POST',
-						url: vm.url,
-						data: vm.getData(),
+						url: url,
+						data: data,
 						headers: {},
-						success: function(data, status, xhr) {
-							if(vm.submit === true && typeof vm.submitCallback == 'function') {
-								vm.submitCallback(ev, vm);
-							}
+						success: function(d, status, xhr) {
+							vm.isLoading = false;
+							successCallback(d);
 						},
-						error: function(data) {
-							if(typeof vm.errorCallback == 'function') {
-								vm.errorCallback(ev, vm, data);
-							}
+						error: function(d) {
+							vm.isLoading = false;
+							vm._callback(ev, 'error', d);
 						}
 					});
 				}
 			}
 			
-			vm.doReset = function(ev) {
-				if(vm.reset===true && typeof vm.resetCallback == 'function') {
-					vm.resetCallback(ev, vm);
+			vm.doSubmit = function(ev) {
+				for(k in vm.$refs) {
+					var comp = vm.$refs[k];
+					if(comp.isValid != undefined && comp.isValid === false) {
+						return;
+					}
 				}
+				if(vm.submitUrl != '') {
+					vm._ajax(vm.submitUrl, vm.getData(), function(d) {
+						vm._callback(ev, 'submit', d);
+					});
+				}else {
+					vm._callback(ev, 'submit', null);
+				}
+			}
+			vm.doReset = function(ev) {
+				vm._callback(ev, 'reset', null);
 			}
 			
 			//对外方法
@@ -70,7 +114,6 @@ define(['avalon', '../base/js/mmRequest', 'text!./td.form.html', 'css!./td.form.
 				}
 				return data;
 			}
-			
 			vm.setData = function(data) {
 				for(k in vm.$refs) {
 					var comp = vm.$refs[k];
@@ -81,7 +124,16 @@ define(['avalon', '../base/js/mmRequest', 'text!./td.form.html', 'css!./td.form.
 			}
 		},
 		$ready: function (vm) {
-      
+      if(vm.loadUrl != '') {
+				vm._ajax(vm.loadUrl, vm.loadParam, function(d) {
+					if(d.rspcod == '200') {
+						vm.setData(d.data);
+					}else {
+						alert('数据加载失败');
+					}
+					vm._trigger(d, 'loaded');
+				});
+			}
     }
 	});
 	
